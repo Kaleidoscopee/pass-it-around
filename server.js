9551e0ad8e2cbc9d648f85a6c79e6cbfcf5e5e81
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-08-17 12:33:10 -0400

    trying to do an i loop down for the repetition

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000; //windows 5000 for mac users
+const drinks = "beer"
 
 
 
+app.get('/get/:number_of_bottles', (req, res)=> {
+    const bottles = req.params.number_of_bottles;
+    const response = (=>(i = bottles, i< bottles, i--));
 
-
+    res.send(`
+        <h1>${bottles} of bottles of ${drinks} on the wall!</h1>
+        <h1>${bottles} bottles of ${drinks}!</h1>
+        <h1>Take one down, pass it around!</h1>
+        <h1>${bottles} bottles of ${drinks} on the wall!</h1>
+        <br />
+    `)
+});
 
 
 
